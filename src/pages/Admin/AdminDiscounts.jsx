21846de6d17cef40acc5e99046d9517ff617a8b5
yash-vaghsiya <yashvaghasiya2006@gmail.com>
2026-07-02 12:22:53 +0530

    sa

--- src/pages/Admin/AdminDiscounts.jsx
+++ src/pages/Admin/AdminDiscounts.jsx
@@ -1,4 +1,4 @@
-asimport React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductsThunk } from "../../features/products/productSlice";
 import { formatCurrency } from "../../utils/format";
